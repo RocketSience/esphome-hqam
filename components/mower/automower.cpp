d4d988c7c02aeeec1617540eccfb88cedfa75649
RocketSience <33906822+RocketSience@users.noreply.github.com>
2026-04-17 23:47:38 +0200

    translated status_messages to german

--- components/mower/automower.cpp
+++ components/mower/automower.cpp
@@ -246,91 +246,91 @@ namespace esphome
             {
             case 0x0010:
                 status = "error";
-                status_message = "Outside working area";
+                status_message = "Außerhalb des Arbeitsbereichs";
                 break;
             case 0x0012:
                 status = "error";
-                status_message = "LBV Low battery voltage";
+                status_message = "Niedrige Batteriespannung";
                 break;
             case 0x001e:
                 status = "error";
-                status_message = "Cutting system blocked";
+                status_message = "Schneidsystem blockiert";
                 break;
             case 0x03EA:
                 status = "mowing";
-                status_message = "MIP Mowing in progress";
+                status_message = "Mähvorgang läuft";
                 break;
             case 0x0006:
                 status = "error";
-                status_message = "Left wheel motor blocked";
+                status_message = "Linker Radmotor blockiert";
                 break;
             case 0x0008:
                 status = "error";
-                status_message = "Right wheel motor blocked";
+                status_message = "Rechter Radmotor blockiert";
                 break;
             case 0x000C:
                 status = "error";
-                status_message = "No loop signal";
+                status_message = "Kein Schleifensignal";
                 break;
             case 0x001A:
                 status = "error";
-                status_message = "Station blocked";
+                status_message = "Ladestation blockiert";
                 break;
             case 0x0022:
                 status = "error";
-                status_message = "Mower lifted";
+                status_message = "Mäher angehoben";
                 break;
             case 0x0034:
                 status = "error";
-                status_message = "Station no contact";
+                status_message = "Kein Kontakt zur Ladestation";
                 break;
             case 0x0036:
                 status = "error";
-                status_message = "Pin expired";
+                status_message = "PIN abgelaufen";
                 break;
             case 0x03E8:
                 status = "mowing";
-                status_message = "Leaving station";
+                status_message = "Verlässt Ladestation";
                 break;
             case 0x03EE:
                 status = "mowing";
-                status_message = "Start mowing";
+                status_message = "Mähen starten";
                 break;
             case 0x03F0:
                 status = "mowing";
-                status_message = "Mowing started";
+                status_message = "Mähen gestartet";
                 break;
             case 0x03F4:
                 status = "mowing";
-                status_message = "Start mowing2";
+                status_message = "Mähen starten 2";
                 break;
             case 0x03F6:
                 status = "docked";
-                status_message = "Charging";
+                status_message = "Ladevorgang";
                 break;
             case 0x03F8:
                 status = "docked";
-                status_message = "Waiting timer2";
+                status_message = "Warten auf Timer 2";
                 break;
             case 0x1016:
                 status = "docked";
-                status_message = "Waiting timer";
+                status_message = "Warten auf Timer";
                 break;
             case 0x0400:
                 status = "docked";
-                status_message = "Parking in station";
+                status_message = "Parken in Ladestation";
                 break;
             case 0x040C:
                 status = "mowing";
-                status_message = "Square mode";
+                status_message = "Quadrat-Modus";
                 break;
             case 0x040E:
                 status = "error";
-                status_message = "Stuck";
+                status_message = "Festgefahren";
                 break;
             case 0x0412:
                 status = "returning";
-                status_message = "Searching";
+                status_message = "Suche läuft";
                 break;
             case 0x0414:
                 status = "paused";
@@ -338,43 +338,43 @@ namespace esphome
                 break;
             case 0x0418:
                 status = "docked";
-                status_message = "Docking";
+                status_message = "Andocken";
                 break;
             case 0x041A:
                 status = "mowing";
-                status_message = "Leaving station";
+                status_message = "Verlässt Ladestation";
                 break;
             case 0x041C:
                 status = "error";
-                status_message = "Error";
+                status_message = "Fehler";
                 break;
             case 0x0420:
                 status = "idle";
-                status_message = "Waiting for use";
+                status_message = "Bereit (Warten auf Einsatz)";
                 break;
             case 0x0422:
                 status = "mowing";
-                status_message = "Follow boundary";
+                status_message = "Folgt Begrenzungskabel";
                 break;
             case 0x0424:
                 status = "returning";
-                status_message = "Found N-Signal";
+                status_message = "N-Signal gefunden";
                 break;
             case 0x0426:
                 status = "error";
-                status_message = "Stuck";
+                status_message = "Festgefahren";
                 break;
             case 0x0428:
                 status = "returning";
-                status_message = "Searching";
+                status_message = "Suche läuft";
                 break;
             case 0x042E:
                 status = "mowing/returning";
-                status_message = "Follow guide line";
+                status_message = "Folgt Leitkabel";
                 break;
             case 0x0430:
                 status = "returning";
-                status_message = "Follow loop wire";
+                status_message = "Folgt Begrenzungsschleife";
                 break;
             default:
                 status = "error";
